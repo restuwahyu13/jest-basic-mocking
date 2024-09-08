3dc61a240f99b3caf1396737290944df94042e07
restuwahyu13 <restuwahyu13@gmail.com>
2024-09-08 12:11:35 +0700

    Change one of unit test case for name

--- __tests__/externalUnmockModule.test.js
+++ __tests__/externalUnmockModule.test.js
@@ -17,14 +17,14 @@ describe('Mocking using external jest.unmock', function () {
 
 		expect(person.get).toBeDefined()
 		expect(jest.isMockFunction(person.get)).toBeFalsy()
-		expect(data).toMatchObject({ name: 'jamal cavalera', age: 25, hobby: 'coding' })
+		expect(data).toMatchObject({ name: 'jamal cavalera v1', age: 25, hobby: 'coding' })
 	})
 
 	it('Result data using person.set', function () {
-		const data = person.set({ name: 'jamal cavalera', age: 25, hobby: 'coding' })
+		const data = person.set({ name: 'jamal cavalera v1', age: 25, hobby: 'coding' })
 
 		expect(person.set).toBeDefined()
 		expect(jest.isMockFunction(person.set)).toBeFalsy()
-		expect(data).toMatchObject({ name: 'jamal cavalera', age: 25, hobby: 'coding' })
+		expect(data).toMatchObject({ name: 'jamal cavalera v1', age: 25, hobby: 'coding' })
 	})
 })
